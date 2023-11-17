fd0ca8f81084dc7f61328c0c87822c2cf91d593a
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-17 21:16:46 +0100

    first commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+Logical Operators
+AND && => ALL OPERANDS ARE TRUE => TRUE
+console.log(true && true); //true
+console.log(true && false); //false
+console.log(false && false); //false
+console.log(true && false && true); //false
+console.log(true && true && true); //true
